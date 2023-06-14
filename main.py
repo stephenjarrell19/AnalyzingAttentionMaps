00c951c29f2d9f172f7aa38b6ca44833198764f2
Stephen Jarrell <stephenjarrell19@gmail.com>
2023-06-13 20:42:00 -0700

    training ready

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ import yaml
 import sys
 from transformers import AutoTokenizer
 from transformers import AutoModelForCausalLM, TrainingArguments, Trainer
-
+from transformers import DataCollatorForLanguageModeling
 from data_processing.arxiv import *
 from data_processing.common_crawl import *
 from data_processing.poems import *
+
+
+
+
+
+def group_texts(examples):
+    block_size = 128
+    # Concatenate all texts.
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of block_size.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
 
 
 def preprocess_function(examples):
@@ -30,10 +50,28 @@ if __name__ == '__main__':
         num_proc=4,
     )
 
+    tokenizer.pad_token = tokenizer.eos_token
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    #lm_dataset = tokenized_dataset.map(group_texts, batched=True, num_proc=4)
     # Get Model
     model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
 
     # Train Model
+    training_args = TrainingArguments(
+        output_dir="arxiv-model",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        weight_decay=0.01,
+    )
 
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["test"],
+        data_collator=data_collator,
+    )
+
+    trainer.train()
 
     # Save Model
