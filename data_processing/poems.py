bce3d7a829260cbdb672ca28d22d0cb9ae753f85
Igotoschoolbybye <87763340+Igotoschoolbybye@users.noreply.github.com>
2023-06-14 22:10:55 -0700

    Update poems.py

---
+++ data_processing/poems.py
@@ -0,0 +1,21 @@
+def preprocess_function(examples):
+    return tokenizer(["".join(x) for x in examples["poem"]])
+  
+block_size = 128
+
+
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+    # customize this part to your needs.
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of block_size.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
